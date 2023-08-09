7e003ac12db694466debbb67cc75b13686278cc8
Gilberto <zzzest@gmail.com>
2023-08-10 00:56:54 +0300

    change begin menu

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,13 +1,23 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
 
     /**
      * Project java-project-lvl1.
+     *
      * @param args input parameters
      */
     public static void main(final String[] args) {
-        Cli.greetingUser();
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet");
+        System.out.println("0 - Exit");
+        System.out.print("Your choice: ");
+        Scanner scanner = new Scanner(System.in);
+        if (scanner.nextLine().equals("1")) {
+            Cli.greetingUser();
+        }
     }
 
 }
