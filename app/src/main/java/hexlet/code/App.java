7690e9447537c13d28eef1a871cab77128fc0678
Gilberto <zzzest@gmail.com>
2023-08-09 22:38:55 +0300

    change greeting app

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,9 +1,15 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to the Brain Games!");
-    }
+        System.out.println("Welcome to the Brain Games!`");
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.print("May I have your name? ");
+        String userName = scanner.next();
+        System.out.println("Hello, " + userName + "!");
+    }
 }
