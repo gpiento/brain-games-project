400fae570cef60aa9f49c090d6a1fd1189450d01
Gilberto <zzzest@gmail.com>
2023-08-28 19:30:16 +0300

    rm package-info.java

--- app/src/main/java/hexlet/code/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * This package contains classes related to user authentication and
- * authorization.  * Some of the classes provide utility methods for working
- * with user accounts.
- */
-package hexlet.code;
-
